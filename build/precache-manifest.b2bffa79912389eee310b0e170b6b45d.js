059be4e831f409c9b936c92f6872fb9194c79c9b
ZeldaPool <smitrana007@gmail.com>
2020-08-24 13:07:30 +0530

    Build

---
+++ build/precache-manifest.b2bffa79912389eee310b0e170b6b45d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17afcdb1ee2f6344a26316f36eaede26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83e3613525ec08368ea3",
+    "url": "/static/css/main.bd2c64de.chunk.css"
+  },
+  {
+    "revision": "bcbbfdb646c625d3f611",
+    "url": "/static/js/2.dda387ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dda387ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e3613525ec08368ea3",
+    "url": "/static/js/main.1f47280c.chunk.js"
+  },
+  {
+    "revision": "6a13587311244584f3ec",
+    "url": "/static/js/runtime-main.e3c88eb8.js"
+  }
+]);
